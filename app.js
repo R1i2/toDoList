1edec470d6589dfb5f1b9f038064936988a69b5e
Rishav <rishavraj8967@gmail.com>
2020-04-20 13:43:42 +0530

    process.env.PORT

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const bodyParser=require("body-parser");
 const app=express();
 const date=require(__dirname+"/views/node.js");
 app.use(express.static("public"));
+app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 const listItems=[];
 const workItems=[];
 let newTodo="";
-app.set("view engine","ejs");
 app.get("/",function(req,res){
     res.render("index",{listItem:date.getDay(),Todo:listItems});
 });
@@ -28,6 +28,6 @@ app.post("/",function(req,res){
         res.redirect("/")
     }
 })
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,function(){
     console.log("port is on and listenning at 3000");
 });
